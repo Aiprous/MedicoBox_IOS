57a7f751178781845414dd02e0baa9016074a252
DhanrajDev <dhanraj4u@gmail.com>
2018-10-03 14:41:05 +0530

    App Icon and Order Place

---
+++ MedicoBox/CustomeCells/Orders/OrderItemsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderItemsTableViewCell.swift
+//  MedicoBox
+//
+//  Created by NCORD LLP on 03/10/18.
+//  Copyright © 2018 Aiprous. All rights reserved.
+//
+
+import UIKit
+
+class OrderItemsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
